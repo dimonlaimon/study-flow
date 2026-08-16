77aa0fc9efba71d4e38d227b6cb84325fd87a85a
dimonlaimon <dimka050707@mail.ru>
2026-08-17 00:14:31 +0300

    Update Schedule.jsx

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -6,7 +6,7 @@ import ScheduleCard from '@/components/schedule/ScheduleCard';
 import WeekNavigator from '@/components/schedule/WeekNavigator';
 
 const GROUP_ID = 45221;
-const API_URL = 'https://ruz.spbstu.ru/api/v1/ruz/scheduler/45221';
+const API_URL = 'https://ruz.spbstu.ru/api/v1/ruz/scheduler';
 
 const dayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
 
