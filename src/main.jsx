a1219a2d170058ae233f64038b88e88674596834
dimonlaimon <dimka050707@mail.ru>
2026-08-17 16:13:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import vkBridge from '@vkontakte/vk-bridge'
 import App from '@/App.jsx'
 import '@/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+// Инициализируем VK Bridge сразу — VK ждёт этот сигнал при загрузке
+vkBridge.send('VKWebAppInit').catch(() => {});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App />
+)
